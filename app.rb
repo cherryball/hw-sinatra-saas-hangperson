97cb94ac194d3f612159016520e34b0a95e8d0a7
cherryball <greencherryball@gmail.com>
2015-11-08 00:09:25 +0000

    cleanup code

--- app.rb
+++ app.rb
@@ -44,27 +44,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    
-   # if letter.nil? or letter.empty?
-    #  flash[:message] = "Invalid guess."
-     # redirect '/show'
-    #end 
-    
-  
-   #    if @game.guess(letter)
-     # if @game.guesses.downcase.include? letter.downcase
-      #    flash[:message] = "You have already used that letter."
-    #  end 
-    #else 
-     # flash[:message] = "Invalid guess."
-    #end
-    
- #   if @game.guesses.include? letter.downcase
-#flash[:message] = "You have already used that letter"
-#elsif !@game.guess(letter)
-#flash[:message] = "Invalid guess."
-#end
-
+ 
     begin
         if !@game.guess(letter) 
           flash[:message] = "You have already used that letter."
@@ -72,7 +52,6 @@ class HangpersonApp < Sinatra::Base
         rescue ArgumentError
             flash[:message] = "Invalid guess."
     end   
-
   
     redirect '/show'
   end
